7557f48948cfa1eb070b91522408b943a8a28644
SaxMan96 <matidorobek@wp.pl>
2018-05-23 22:40:33 +0200

    Added exceptions in graph construction methods

--- src/Application.scala
+++ src/Application.scala
@@ -3,9 +3,6 @@ import java.nio.charset.CodingErrorAction
 import scala.io.Codec
 
 //TODO Dokumentacja JavaDoc
-//TODO Plik wejœciowy + ew. generacja
-//TODO Czytanie wejœcia
-//TODO sprawdzamy w funkcjach typu add, ¿eby sprawdziæ, czy istnieje ju¿ taki wierzcho³ek
 //TODO sprawdzamy nadpisanie
 //TODO W przypadku heurystyk sprawdzamy czy heurystyka odwrotna ju¿ istnieje to zg³aszamy b³¹d nawet je¿eli ma dobr¹ wartoœæ
 
@@ -13,29 +10,35 @@ object obj {
   def main(args: Array[String]) {
     var g = new Graph[String, Int](Map())
     readInput
-    g.showVertices
-    g.showEdges
-    g.showHeuristics
-    
+//    g.showVertices
+//    g.showEdges
+//    g.showHeuristics
+
     def readInput() {
       val decoder = Codec.UTF8.decoder.onMalformedInput(CodingErrorAction.IGNORE)
       val filename = "files/cities.txt";
       val bufferedSource = Source.fromFile(filename)(decoder)
       var readMode = 0; //1 Vertices, 2 Edges, 3 Heuristics
-      for (line <- bufferedSource.getLines) {
-        if (line.equals("Vertices")) readMode = 1;
-        else if (line.equals("Edges")) readMode = 2;
-        else if (line.equals("Heuristics")) readMode = 3;
-        else {
-          readMode match {
-            case 1 => g = g.addVertex(line)
-            case 2 => g = g.addEdge(line.split("\\s+")(0), line.split("\\s+")(1), line.split("\\s+")(2).toInt)
-            case 3 => g = g.addHeur(line.split("\\s+")(0), line.split("\\s+")(1), line.split("\\s+")(2).toInt)
+      var lineCtr = 1;
+      try {
+        for (line <- bufferedSource.getLines) {
+          if (line.equals("Vertices")) readMode = 1;
+          else if (line.equals("Edges")) readMode = 2;
+          else if (line.equals("Heuristics")) readMode = 3;
+          else {
+            readMode match {
+              case 1 => g = g.addVertex(line, lineCtr)
+              case 2 => g = g.addEdge(line.split("\\s+")(0), line.split("\\s+")(1), line.split("\\s+")(2).toInt, lineCtr)
+              case 3 => g = g.addHeur(line.split("\\s+")(0), line.split("\\s+")(1), line.split("\\s+")(2).toInt, lineCtr)
+            }
           }
+          lineCtr += 1
         }
+      } catch {
+        case iae: IllegalArgumentException => iae.printStackTrace()
       }
       bufferedSource.close
-   }
+    }
 
     //    val ver = g.addVertex("Warszawa").addVertex("P³ock").addVertex("Starachowice")
     //    val edg = ver.addEdge("Warszawa", "P³ock", 10).addEdge("Starachowice", "P³ock", 15)
--- src/Graph.scala
+++ src/Graph.scala
@@ -1,43 +1,61 @@
 import scala.collection.immutable.Map
+import scala.collection.immutable.List
 
-case class Vertex[VertexID,DistanceType](edgesList: List[Edge[VertexID, DistanceType]], heuristicMap: Map[VertexID, DistanceType])
-case class Edge[VertexID,DistanceType](to: VertexID, distance: DistanceType)
+case class Vertex[VertexID, DistanceType](edgesList: List[Edge[VertexID, DistanceType]], heuristicMap: Map[VertexID, DistanceType])
+case class Edge[VertexID, DistanceType](to: VertexID, distance: DistanceType)
 
-class Graph[VertexID,DistanceType](vertices: Map[VertexID, Vertex[VertexID, DistanceType]]) {
-  def addVertex(vertexID: VertexID) = new Graph[VertexID, DistanceType](vertices + (vertexID -> new Vertex[VertexID, DistanceType](List(), Map())))
-  def showVertices = for (v <- vertices) println(v._1)
-  def addEdge(from: VertexID, to: VertexID, distance: DistanceType) =
+class Graph[VertexID, DistanceType](vertices: Map[VertexID, Vertex[VertexID, DistanceType]]) {
+  def addVertex(vertexID: VertexID, lineCtr: Int) = {
+    if (findVertex(vertexID))
+      throw new IllegalArgumentException("addVertex() error at line " + lineCtr + ": \"" + vertexID + "\" that vertex already exist.")
+    new Graph[VertexID, DistanceType](vertices + (vertexID -> new Vertex[VertexID, DistanceType](List(), Map())))
+  }
+
+  def addEdge(from: VertexID, to: VertexID, distance: DistanceType, lineCtr: Int) = {
+    if (!findVertex(from))
+      throw new IllegalArgumentException("addEdge() error at line " + lineCtr + ": \"" + from + "\" that vertex does not exist.")
+    if (!findVertex(to))
+      throw new IllegalArgumentException("addEdge() error at line " + lineCtr + ": \"" + to + "\" that vertex does not exist.")
+    if (findEdgeOrHeuristic(from, to))
+      throw new IllegalArgumentException("addEdge() error at line " + lineCtr + ": \"" + from + "\"-->\"" + to + "\" that edge does exist. You are overwriting it.")
     new Graph[VertexID, DistanceType](vertices + (from -> Vertex[VertexID, DistanceType]((vertices(from).edgesList :+ new Edge[VertexID, DistanceType](to, distance)), Map())))
-  def showEdges = for (
-    v <- vertices;
-    e <- v._2.edgesList
-  ) println(v._1 + " " + e.to + " " + e.distance)
-  def addHeur(from: VertexID, to: VertexID, heuristic: DistanceType) =
+  }
+
+  def addHeur(from: VertexID, to: VertexID, heuristic: DistanceType, lineCtr: Int) = {
+    if (!findVertex(from))
+      throw new IllegalArgumentException("addHeur() error at line " + lineCtr + ": \"" + from + "\" that vertex does not exist.")
+    if (!findVertex(to))
+      throw new IllegalArgumentException("addHeur() error at line " + lineCtr + ": \"" + to + "\" that vertex does not exist.")
+    if (findEdgeOrHeuristic(from, to))
+      throw new IllegalArgumentException("addHeur() error at line " + lineCtr + ": \"" + from + "\"<-->\"" + to + "\" that edge does exist. You are overwriting it.")
     new Graph[VertexID, DistanceType](vertices + (from -> Vertex[VertexID, DistanceType]((vertices(from).edgesList), vertices(from).heuristicMap + (to -> heuristic))))
-  def showHeuristics = for (
-    v <- vertices;
-    h <- v._2.heuristicMap
-  ) println(v._1 + " " + h._1 + " " + h._2)
-  
+  }
+
+  def findVertex(vertexID: VertexID): Boolean = {
+    vertices.contains(vertexID)
+  }
+  def findEdgeOrHeuristic(from: VertexID, to: VertexID): Boolean = {
+    for (v <- vertices)
+      if (v._1.equals(from) && (
+        v._2.edgesList.exists(edge => edge.to.equals(to)) ||
+        v._2.heuristicMap.exists(heur => heur._1.equals(to))))
+        return true
+    return false
+  }
+
+  def showVertices = {
+    for (v <- vertices) println(v._1)
+  }
+  def showEdges = {
+    for (
+      v <- vertices;
+      e <- v._2.edgesList
+    ) println(v._1 + " " + e.to + " " + e.distance)
+  }
+  def showHeuristics = {
+    for (
+      v <- vertices;
+      h <- v._2.heuristicMap
+    ) println(v._1 + " " + h._1 + " " + h._2)
+  }
 }
-
-
-
-//
-//
-//
-//class Graph(someNodes:Set[N], val edges:Set[(N,N)]) {
-//
-//  // someNodes does not necessarily contains all nodes from the edges, nodes does
-//  val nodes: Set[N] = someNodes ++ edges.map(_._1) ++ edges.map(_._2)
-//
-//  def addVortex(node:N) = new Graph(nodes + node, edges)
-//
-//  def addEdge(edge: (N, N)) = new Graph(nodes + edge._1 + edge._2, edges + edge)
-//
-//  // Redundant, but more efficient data structure for the outgoing edges
-//  lazy val outgoingEdges: Map[N, Set[N]] = edges.groupBy(_._1).mapValues(s => s.map(_._2))
-//
-//  // Here come your algorithms
-//}
-//
